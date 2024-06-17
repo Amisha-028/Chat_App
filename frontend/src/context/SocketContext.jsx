a1c793caf797b338dc34dd3505e2bd36c6f49e2b
amisha-028 <iamisha0018@gmail.com>
2024-06-17 23:40:07 +0530

    socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://chat-app-db-yo4l.onrender.com/login", {
 				query: {
 					userId: authUser._id,
 				},
